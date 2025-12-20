ba2ea78e1d47f182774d912ad3a9500f6607bf89
D-Mastrocola <mastrodomcola@gmail.com>
2025-12-19 23:12:00 -0500

    setup .net environment

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Mix_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Mix Calculations";
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
